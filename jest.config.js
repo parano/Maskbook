a70b5d710925be0591479d89bdca542d82214ac4
guanbinrui <guanbinrui@dimension.im>
2020-02-24 13:07:19 +0800

    chore: support unit test with jest and puppeteer

--- jest.config.js
+++ jest.config.js
@@ -20,3 +20,22 @@ module.exports = {
         'idb/with-async-ittr': require.resolve('idb/with-async-ittr-cjs.js'),
     },
 }
+
+module.exports = {
+    preset: 'jest-puppeteer',
+
+    // globalSetup: path.join(__dirname, './scripts/jest-global-setup'),
+    // setupFiles: [require.resolve('fake-indexeddb/auto'), path.join(__dirname, './scripts/jest-setup.js')],
+
+    // skip packages other than 'ts-results' and 'async-call-rpc'
+    transformIgnorePatterns: ['node_modules((?!(ts-results|async-call-rpc)).)*$'],
+    transform: {
+        '.*\\.tsx?': 'ts-jest',
+        '[/\\\\]node_modules[/\\\\].+\\.m?js$': 'jest-esm-transformer',
+    },
+    moduleNameMapper: {
+        '^@holoflows/kit.+$': require.resolve('@holoflows/kit/umd/index.js'),
+        'lodash-es': require.resolve('lodash'),
+        'idb/with-async-ittr': require.resolve('idb/with-async-ittr-cjs.js'),
+    },
+}
